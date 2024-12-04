d2117d7749c23a9162e7d3d26744286fe5b54275
Rodobrodo2 <rodondolo2@gmail.com>
2024-12-04 16:59:45 +0100

    Da Oggetto document a Modifica l'html

--- esercizi/index.js
+++ esercizi/index.js
@@ -1,2 +1,23 @@
 // 1.0 Oggetto document
 console.log(document);
+
+// 2.0 getElementById
+const $paragrafo = document.getElementById("paragrafo");
+console.log($paragrafo);
+
+// 3.0 querySelector
+const $titolo = document.querySelector("#titolo");
+console.log($titolo);
+
+// 4.0 Modifica il testo di un paragrafo
+// 5.0 cambia lo stile
+function modificaTesto() {
+    $paragrafo.innerText = "Il testo e stato modificato!";
+    $paragrafo.style.backgroundColor = "yellow";
+    $paragrafo.style.fontSize = "30px";
+}
+modificaTesto();
+
+// 6.0 Modifica l'HTML
+const $button = document.querySelector("#mio");
+$button.innerHTML = "<button> Ero un P </button>"
